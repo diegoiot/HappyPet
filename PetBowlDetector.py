189bd27c230beadf6928519984fab31815795002
diegoiot <74726357+diegoiot@users.noreply.github.com>
2022-03-23 15:44:51 -0300

    Update PetBowlDetector.py

--- PetBowlDetector.py
+++ PetBowlDetector.py
@@ -2,7 +2,7 @@ import jetson.inference
 import jetson.utils
 
 net = jetson.inference.detectNet("ssd-mobilente-v2",["--model=/PetBowlDetector/ssd-mobilenet.onnx","--labels=/PetBowlDetector/labels.txt","--input-blob=input_0","--output-cvg=scores","--output-bbox=boxes"])
-camera = jetson.utils.videoSource("rtsp://USER:KEYCAM@IPLAN:554/H.264")
+camera = jetson.utils.videoSource("rtsp://USER:KEYCAM@IPCAM_LAN:554/H.264")
 display = jetson.utils.videoOutput("display://0")
 
 while display.IsStreaming():
